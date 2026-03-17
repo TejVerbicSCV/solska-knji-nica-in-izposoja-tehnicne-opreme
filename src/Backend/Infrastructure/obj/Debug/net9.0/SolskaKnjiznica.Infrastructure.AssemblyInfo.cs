b822fdc15099420294fbc81c62b833ceabcbef24
Tej Verbic SCV <tej.verbic@scv.si>
2026-03-17 08:59:59 +0100

    povezava z  bazo

--- src/Backend/Infrastructure/obj/Debug/net9.0/SolskaKnjiznica.Infrastructure.AssemblyInfo.cs
+++ src/Backend/Infrastructure/obj/Debug/net9.0/SolskaKnjiznica.Infrastructure.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolskaKnjiznica.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7e695ed4cbddb47bd6741c7b89450bef60eebc")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98906d11aeb458b21a1c49f35f38e822c468c5d1")]
->>>>>>> c1d1336091f4613b1b028b18ac175abab6bd916c
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4912fe47922cbacaa45fe0fdaaf6459e3ff600cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolskaKnjiznica.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolskaKnjiznica.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
